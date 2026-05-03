e68f0f37a01dd2e184b51aec2ff1915d81884214
geonsang-jo <if.geon@gmail.com>
2026-05-03 16:56:35 +0900

    feat: add code copy button and GoogleAnalytics component
    - Add CodeBlock component wrapping <pre> with hover copy button
    - Register CodeBlock as mdx pre component
    - Add reusable GoogleAnalytics component using next/script

--- app/components/mdx/index.ts
+++ app/components/mdx/index.ts
@@ -1,7 +1,9 @@
 import { Timeline, TimelineItem } from "./Timeline";
 import { FileTree, Folder, File } from "./FileTree";
+import { CodeBlock } from "./CodeBlock";
 
 export const mdxComponents = {
+  pre: CodeBlock,
   Timeline,
   TimelineItem,
   FileTree,
