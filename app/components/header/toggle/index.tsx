26bf3625ef4bdbad490547b185f02888329a2ebe
geonsang-jo <if.geon@gmail.com>
2026-05-04 18:34:48 +0900

    fix: swap dark/light mode toggle icons to show correct state

--- app/components/header/toggle/index.tsx
+++ app/components/header/toggle/index.tsx
@@ -55,9 +55,9 @@ function NavToggles() {
         onClick={changeTheme}
       >
         {mounted && resolvedTheme === "dark" ? (
-          <MoonIcon className="h-4 w-4" />
-        ) : (
           <SunIcon className="h-4 w-4" />
+        ) : (
+          <MoonIcon className="h-4 w-4" />
         )}
       </Button>
     </nav>
